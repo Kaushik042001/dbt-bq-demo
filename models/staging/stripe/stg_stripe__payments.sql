7ed126446ccb775ecd062ce5ff936663d1f1bcf0
Kaushik Chari <kaushikchari1404@gmail.com>
2025-02-19 10:07:36 +0000

    implemented macros, added test cases and generated docs

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,6 +3,6 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    {{ cents_to_dollors('payment_amount', 4) }} as amount,
+    {{ cents_to_dollors('amount', 4) }} as amount,
     created as created_at
 from {{ source('stripe', 'payment') }}
